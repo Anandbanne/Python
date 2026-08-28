0c39e53124c46e5998487aadae3debb613f09044
Anand Banne <anandbanne43@gmail.com>
2026-08-28 22:28:12 +0530

    count number of digits by given number

---
+++ for_loop/level_3/count_prime_digits.py
@@ -0,0 +1,14 @@
+n=int(input())
+count=0
+for i in str(n):
+    dgt=int(i)
+    if dgt<=1:
+        continue
+    found=True
+    for j in range(2,dgt):
+        if dgt%j==0:     
+            found=False
+            break
+    if found:
+        count+=1
+print(count)
