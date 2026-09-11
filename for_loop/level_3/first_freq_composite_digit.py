3613060d46da89c8be9f65e35a4ccc40087004e0
Anand Banne <anandbanne43@gmail.com>
2026-09-11 12:33:54 +0530

    Add files via upload

---
+++ for_loop/level_3/first_freq_composite_digit.py
@@ -0,0 +1,25 @@
+n=int(input())
+freq=[0]*10
+
+for i in str(n):
+    dgt=int(i)
+    freq[dgt]+=1
+high=-1
+res=-1
+for dgt in range(10):
+    if dgt<=1:
+        continue
+    is_prime=True
+    for k in range(2,dgt):
+        if dgt%k==0:
+            is_prime=False
+            break
+    if not is_prime and freq[dgt]!=0:
+        if freq[dgt]>high:
+            high=freq[dgt]
+            res=dgt
+print(freq)
+if res==-1:
+    print("No composite digit.")
+else:
+    print(res)
