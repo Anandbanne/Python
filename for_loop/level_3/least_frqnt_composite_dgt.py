2a2a57e55d70bd3bedef09f576fcba4f20891909
Anand Banne <anandbanne43@gmail.com>
2026-09-12 06:44:20 +0530

    Add files via upload

---
+++ for_loop/level_3/least_frqnt_composite_dgt.py
@@ -0,0 +1,25 @@
+n=int(input())
+freq=[0]*10
+
+for i in str(n):
+    dgt=int(i)
+    freq[dgt]+=1
+low=float('inf')
+res=-1
+for dgt in range(10):
+    if dgt<=1:
+        continue
+    is_prime=True
+    for k in range(2,dgt):
+        if dgt%k==0:
+            is_prime=False
+            break
+    if not is_prime and freq[dgt]!=0:
+        if freq[dgt]<low:
+            low=freq[dgt]
+            res=dgt
+print(freq)
+if res==-1:
+    print("No composite digit.")
+else:
+    print(res)
